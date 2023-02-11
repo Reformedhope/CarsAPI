aadd233dd8b6829a3d8f6fb433abace467d08dc0
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-02-11 13:55:47 -0500

    Created Car Model for Cars app + registered cards and Rest Framework in Project Settings Installed Apps Section

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class car(models.Model):
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=8,decimal_places=2) # doesnt round in unpredictable ways. Have to include how many digits are going to be in this feild
