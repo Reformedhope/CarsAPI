9fec883a274f35d7061744ba91457dd3841b9fe0
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-02-12 10:42:06 -0500

    updated project URLS plus created first view function + created Created cars app URLS file

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view # for every function we create we are going to put this decorator right above that function.
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def cars_list(request):
+
+    return Response('ok')
+
